fde45e906dce365fbab7cddd84453862509dc002
radovan.liska <radovan.liska@notino.com>
2023-01-06 08:14:22 +0100

    initial commit

---
+++ hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Structure ")
+}
+
+// https://www.bogotobogo.com/GoLang/GoLang_Modules_1_Creating_a_new_module.php
+// https://go.dev/doc/gopath_code
+// https://go.dev/doc/code
