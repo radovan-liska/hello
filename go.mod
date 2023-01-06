13e918ca2e4b25dc250603ce337cbdb7c7746c6b
radovan.liska <radovan.liska@notino.com>
2023-01-06 09:28:07 +0100

    added first module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/radovan-liska
+
+go 1.19
--- hello/hello.go
+++ hello/hello.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	stringutil "github.com/radovan-liska/reverse"
+)
 
 func main() {
-	fmt.Println("Structure ")
+	fmt.Println(stringutil.Reverse("Radovan"))
 }
 
 // https://www.bogotobogo.com/GoLang/GoLang_Modules_1_Creating_a_new_module.php
